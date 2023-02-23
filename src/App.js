02806924559a61d25a44208792cb40547200d7d9
ilham <ilhamksyuriadi@gmail.com>
2023-02-24 05:15:34 +0700

    put to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import TodoItem from './TodoItem';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='landing'>
+        <div>
+          What is Lorem Ipsum?
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+        <div>
+          <img alt="temp-imag" src='https://picsum.photos/200/300'>
+          </img>
+        </div>
+      </div>
+      <TodoItem id={1} content="Learn how to design" />
+      <TodoItem id={2} content="Go to the gym" />
+      <TodoItem id={3} content="Launch first application" />
+      <TodoItem id={4} content="Try cooking" />
+      <TodoItem id={5} content="Attemp online course" />
+      <TodoItem id={6} content="Preparation for the interview" />
     </div>
   );
 }
